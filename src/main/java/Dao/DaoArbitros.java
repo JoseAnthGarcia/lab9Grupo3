f06d562c136d11fa978ae9fd1c2a80423583a371
Katherine Obregon <a20181823@pucp.edu.pe>
2020-12-01 19:17:53 -0500

    daobase

--- src/main/java/Dao/DaoArbitros.java
+++ src/main/java/Dao/DaoArbitros.java
@@ -3,15 +3,36 @@ package Dao;
 import Bean.Arbitros;
 
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class DaoArbitros  {
+public class DaoArbitros extends DaoBase {
     public ArrayList<Arbitros> listarArbitros() {
 
         ArrayList<Arbitros> arbitros = new ArrayList<>();
 
-        /*
-                Inserte su código aquí
-                 */
+        try(Connection conn = getConnection();
+            Statement stmt = conn.createStatement();) {
+
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM sw1lab8.arbitros;");
+
+            while (rs.next()) {
+
+                department.setDepartmentId(rs.getInt(1));
+                department.setDepartmentName(rs.getString(2));
+                department.setManagerId(rs.getInt(3));
+                department.setLocationId(rs.getInt(4));
+
+
+                listaDepartamentos.add(department);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return arbitros;
     }
---
+++ src/main/java/Dao/DaoBase.java
@@ -0,0 +1,19 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DaoBase {
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        String user= "root";
+        String pssword="root";
+        String url="jdbc:mysql://localhost:3306/hr?serverTimezone=America/Lima";
+        return  DriverManager.getConnection(url,user, pssword);
+    }
+}
